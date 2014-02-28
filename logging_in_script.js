b3e882d7632bfd860311cea565e334649cee0d2b
computermad93 <c.murrell93@live.co.uk>
2014-02-28 15:44:41 +0000

    Update logging_in_script.js

--- logging_in_script.js
+++ logging_in_script.js
@@ -5,14 +5,14 @@ $(document).ready(function()
 		//remove all the class add the messagebox classes and start fading
 		$("#msgbox").removeClass().addClass('messagebox').text('Validating....').fadeIn(1000);
 		//check the username exists or not from ajax
-		$.post("http://192.168.1.83/smallt/login_script.php",{ user_name:$('#username').val(),password:$('#password').val(),rand:Math.random() } ,function(data)
+		$.post("http://192.168.1.83/smallt/login_script.php",{ userName:$('#userName').val(),password:$('#password').val(),rand:Math.random() } ,function(data)
         {
 		  if(data=='yes') //if correct login detail
 		  {
 		  	$("#msgbox").fadeTo(200,0.1,function()  //start fading the messagebox
 			{ 
 			  //add message and change the class of the box and start fading
-			  $(this).html('Logging in.....').addClass('messageboxok').fadeTo(900,1,
+			  $(this).html('Logging in...').addClass('messageboxok').fadeTo(900,1,
               function()
 			  { 
 			  	 //redirect to secure page
